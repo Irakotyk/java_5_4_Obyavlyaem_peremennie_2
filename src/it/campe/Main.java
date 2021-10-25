da308ebb5cac618e6aa6ef5f11a9744cd4c4efd3
Irakotyk <93030438+Irakotyk@users.noreply.github.com>
2021-10-25 12:01:09 +0200

    task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,6 +3,12 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int a,b;
+        a=4;
+        b=3;
+        System.out.print("a=4,b=3");
+
+
+
     }
 }
